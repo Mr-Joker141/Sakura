0694db11fd30b5f2e87e18e3e55114932c7ec929
Nadun <nadunsewmina@gmail.com>
2021-09-13 00:46:59 +0530

    7th commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,5 +21,5 @@ const userSchema =new mongoose.Schema({
 );
 
 
-const Users =mongoose.model("Users",userSchema);
-export default Users;
+const User =mongoose.model("User",userSchema);
+export default User;
